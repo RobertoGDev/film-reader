fd5dc0d5a2aade33abae5d787082732b95db10bf
Roberto <rob.vicalvaro@gmail.com>
2018-07-29 22:04:50 +0200

    Funcionando index y routes | maquetado launcher

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET launcher. */
 router.get('/', function(req, res, next) {
-  res.render('launcher', { title: 'Film-reader' });
+  res.render('launcher', { section: 'launcher', title: 'Film-reader' });
+});
+
+/* GET feed. */
+router.get('/index', function(req, res, next) {
+  res.render('index', { section: 'catalogo', title: 'Catálogo' });
+});
+
+/* GET feed. */
+router.get('/films', function(req, res, next) {
+  res.render('index', { section: 'films', title: 'Films' });
+});
+
+/* GET feed. */
+router.get('/tv', function(req, res, next) {
+  res.render('index', { section: 'tv', title: 'Series' });
 });
 
 module.exports = router;
